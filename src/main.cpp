01d5532ab822347e31adc083a732cf7cdeb69ee7
zanderp25 <zanderpicon@outlook.com>
2023-11-22 01:50:44 +0000

    Add time zone and duration to events
    Co-authored-by: Jordan <jordanrobbedthesun@users.noreply.github.com>
    Co-authored-by: MrL935 <MrL935@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <array>
 #include <iomanip>
-// #include "event.h"
+#include "event.h"
 #include "calendar.h"
 using namespace std;
 
 int main(){
-    Month m(1, 2023);
+    Month m(1, 2023); 
     cout << m;
-
-    Event party(2023, 11, 16, 20, 30, "Party time ツ", "test", "test");
+    
+    Event party(2023, 11, 16, 20, 30, 0.07, -500 , "party time! ツ", "test", "test");
     cout << party << endl;
     cout << party.getEventDate() << endl;
-    cout << party.getEventTime() << endl;
+    cout << party.getEventTimeString() << endl;
+
+    Event upsideDownParty(2023, 11, 16, 20, 30, 5.23, 930 , "(: ¡ǝɯıʇ ʎʇɹɐd", "ʇsǝʇ", "ʇsǝʇ");
+    cout << "\n" << upsideDownParty << endl;
     return 0;
 }
